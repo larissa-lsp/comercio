081c41bc1fb868707a20dcc6674109b7b8b807dc
Larissa Paixão <larissapaixaolsp@gmail.com>
2023-06-13 09:15:51 -0300

    Edicao template produto

--- src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
@@ -1,17 +1,34 @@
 package com.itb.inf2dm.comercio.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itb.inf2dm.comercio.model.Produto;
 
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
 	
+	List<Produto> listaDeProdutos = new ArrayList<Produto>();
+	
 	@GetMapping("/listar")
 	public String listarProdutos() {
+		Produto p1 = new Produto();
+		p1.setId(20l);
+		p1.setNome("Máquina de Lavar Brastemp 12 litros");
+		p1.setCodigoBarras("HUKFEAHKIFBIHFUHF");
+		p1.setPreco(3799.99)
 		
+		
+		listaDeProdutos.add(p1);
 		return "produtos";
-		
+	}
+	
+	@GetMapping("/novo-prod")
+	public String novoProduto() {
+		return "novo-prod";
 	}
 }
--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -1,5 +1,54 @@
 package com.itb.inf2dm.comercio.model;
 
 public class Produto {
+	private Long id;
+	private String descricao;
+	private String nome;
+	private String codigoBarras;
+	private double preco;
+	
+	// public: Modificador de acesso livre para todas as classes.
+	// private: Modificador de acesso restrito apenas para membros da propria classe.
+	//			Entenda-se por "membros" (atributos e/ou métodos)
+	
+	// Métodos Setter's e Getter's: Adicionar e Recuperar dados do atributo "RESPECTIVAMENTE"
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public double getPreco() {
+		return preco;
+	}
 
 }
